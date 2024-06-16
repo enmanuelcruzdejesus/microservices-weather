af82cac798de308d0410a49d6bf9ce6ec03203ab
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2024-06-16 16:20:44 -0400

    adding temperature endpoints

--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,4 +1,5 @@
 using CloudWeather.Temperature.DataAcces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,24 @@ builder.Services.AddDbContext<TemperatureDbContext>(opts =>{
 
 var app = builder.Build();
 
+
+
+app.MapGet("/observation/{zip}",(string zip, [FromQuery] int? days, TemperatureDbContext context )=>{
+    if(days == null || days < 1 || days > 0) Results.BadRequest("");
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = context.Temperature
+                  .Where(p => p.ZipCode == zip && p.CreatedOn > startDate)
+                  .ToListAsync();
+
+    return Results.Ok(zip);
+});
+
+app.MapPost("/obsevations",async(Temperature temp,  TemperatureDbContext context) =>{
+    temp.CreatedOn = temp.CreatedOn.ToUniversalTime();    
+    await context.AddAsync(temp);
+    await context.SaveChangesAsync();
+    
+});
 
 app.Run();
 
